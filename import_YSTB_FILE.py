9ff845bbdd1c3297515147c6514efb9d89980427
jyx <jyxjyx1234@outlook.com>
2024-08-01 14:37:47 +0800

    上传

---
+++ import_YSTB_FILE.py
@@ -0,0 +1,30 @@
+from YSTB_FILE import *
+import os
+
+ori_path = "ysbin_new/"
+trans_path = "triline_text_trans/"
+out_path = "scr_trans/"
+
+try:
+    key = open("Key.txt","r",encoding='utf8').readlines()
+    encrypt = eval(key[0])
+except:
+    encrypt = 0x00000000
+
+filelist = os.listdir(trans_path)
+
+for filename in filelist:
+    YSTB_f = YSTB_FILE(path=ori_path + filename.replace(".tra.txt", ""))
+    trans_f = open(trans_path + filename, "r", encoding="utf8")
+
+    for l in trans_f:
+        if len(l) > 0 and l[0] == "[":
+            command_offset = int(l[1:-2])
+        elif l[0:4] == "TR2=":
+            transtext = l[4:-1]
+
+            YSTB_f.append_trans(command_offset, transtext)
+
+    YSTB_f.save_file(out_path + filename.replace(".tra.txt", ""),encrypt=encrypt)
+
+os.system("copy scr_trans\\* Release\\ysbin\\")
